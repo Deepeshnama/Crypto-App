e2a74be76324978ca44a3a7028c21d4adfa7da0c
Deepeshnama <deepeshnama0786@gmail.com>
2024-05-24 18:08:11 +0530

    Api Changes

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const API_URL = "/api/user";
+// const API_URL = "/api/user";
 
 // Register Service
 
 const register = async (formData) => {
-  const response = await axios.post(API_URL + "/register", formData);
+  const response = await axios.post("https://authentication-2-qgze.onrender.com/api/user/register", formData);
   localStorage.setItem("user", JSON.stringify(response.data));
   return response.data;
 };
@@ -14,7 +14,7 @@ const register = async (formData) => {
 // Login Service
 
 const login = async (formData) => {
-  const response = await axios.post(API_URL + "/login" , formData)
+  const response = await axios.post("https://authentication-2-qgze.onrender.com/api/user/login" , formData)
   localStorage.setItem("user", JSON.stringify(response.data));
   return response.data;
 }
